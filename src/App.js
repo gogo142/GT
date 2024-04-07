a154654ce9a2ecc4d0a063d6ef3890c13c562bda
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2024-04-07 14:19:47 +0200

    added herosection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import Header from './components/Header';
+import bg from './Images/cars.jpg'
 
 function App() {
-  return (
-    <div className="App">
 
+  const style = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center', 
+  }
+
+  return (
+    <main className="h-screen w-screen" style={style}>
+     <Header />
+
+    <div className='h-full w-full flex flex-col justify-center px-[4rem]'>
+      <h1 className='text-[#98ec65] font-bold text-[5rem]'>Creative</h1>
+      <h3 className='text-white text-[2rem] font-bold'>design studio</h3>
+
+      <button className='w-[10rem] py-[6px] bg-[#98ec65] rounded-3xl hover:bg-[#81e047] mt-[1rem]'>Explore Now</button>
     </div>
+
+    </main>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute w-full h-[4rem] py-4'>
+        <ul className='flex items-center justify-center space-x-10 w-full px-6'>
+            <li className='text-[#98EC65] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse'>Home</li>
+            <li className='text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse'>About</li>
+            <li className='text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse'>Contact Us</li>
+        </ul>
+        
+    </div>
+  )
+}
+
+export default Header
